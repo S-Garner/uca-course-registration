00a31c1c012414a2aa84b847ff1854ffbccedc5c
Seth Garner <sgarner@cub.uca.edu>
2025-11-01 18:34:29 -0500

    fix: Removed unused imports

--- src/main/java/edu/uca/registration/repo/Repository.java
+++ src/main/java/edu/uca/registration/repo/Repository.java
@@ -2,8 +2,6 @@ package edu.uca.registration.repo;
 
 import java.util.Map;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-
 import edu.uca.registration.model.Registrable;
 
 public interface Repository<T extends Registrable> {
